c2982c5fb061a52745a2050ba39a8c0d0be30393
7371zaa <nastya.zlygosteva.00@mail.ru>
2021-04-18 01:32:56 -0700

    Add 5 LR

---
+++ SP/5/main.cpp
@@ -0,0 +1,59 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+#include <vector>
+#include <thread>
+
+using namespace std;
+
+void copy_file(string path_src_file, string path_dst_file){
+    char* buffer = new char;
+ 
+    ifstream infile(path_src_file, ios::binary);
+    ofstream outfile(path_dst_file, ios::binary);
+    
+    while (!infile.eof()){
+        infile.read(buffer, sizeof(char));
+        outfile.write(buffer, sizeof(char));
+    }
+ 
+    cout << "Copied " << path_src_file << " to " << path_dst_file << endl;
+ 
+    delete buffer;
+}
+
+int main(int argc, char const *argv[])
+{
+    string path_from = argv[1];
+    string path_to = argv[2];
+    
+    vector<thread> th_handlers;
+
+    for (const auto & entry : filesystem::directory_iterator(path_from)){
+        string filename = entry.path().filename();
+
+        if (filesystem::is_directory(entry.symlink_status())){
+            cout << filename << " is directory, skipping" << endl;
+            continue;
+        }
+
+        string copy_from = path_from + '/'+ filename;
+        string copy_to = path_to + '/'+ filename;
+
+        th_handlers.push_back(
+            thread(
+                copy_file,
+                copy_from,
+                copy_to
+            )
+        );
+
+    }
+
+    for (int i = 0; i < th_handlers.size(); ++i)
+        th_handlers[i].join();
+
+	return 0;
+}
